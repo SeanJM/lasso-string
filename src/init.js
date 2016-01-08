3fcb6f779f87148d4ceedcf8f8556918c72db951
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-01-08 13:56:02 -0500

    forgot to rename one instance of 'rope'

--- src/init.js
+++ src/init.js
@@ -1,5 +1,5 @@
 for (var k in lasso.fn) {
-  rope[k] = function (k) {
+  lasso[k] = function (k) {
     return function (string) {
     	var strung = { value : string };
       var a = [].slice.call(arguments, 1);
