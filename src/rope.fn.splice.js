ba1988b57fecc348dc07ccd93191cc8d7a38cd9b
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-01-07 19:19:54 -0500

    Improve performance of ‘splice’

--- src/rope.fn.splice.js
+++ src/rope.fn.splice.js
@@ -1,7 +1,4 @@
 rope.fn.splice = function (strung, start, length, newString) {
-  var s = strung.value;
-  var a = s.substr(0, start);
-  var b = s.substr(start + length, s.length - start - length);
-  strung.value = a + newString + b;
+  strung.value = strung.value.substr(0, start) + newString + strung.value.substr(start + length, strung.value.length - start - length);
   return strung;
 };
