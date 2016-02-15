bce29411f606a2b82a614a60392cbf7fe3e90251
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-02-15 15:48:06 -0500

    Simplified implementation of 'timStart' and 'trimEnd'

--- src/lasso.trimEnd.js
+++ src/lasso.trimEnd.js
@@ -1,9 +1,3 @@
 lasso.trimEnd = function (string) {
-  var n = string.length - 1;
-  var s = string.substr(n, 1);
-  while (s === ' ' || s === '\n' || s === '\t' || s === '\r') {
-    n -= 1;
-    s = string.substr(n, 1);
-  }
-  return string.substr(0, n + 1);
+  return string.replace(/\s+$/, '');
 };
--- src/lasso.trimStart.js
+++ src/lasso.trimStart.js
@@ -1,9 +1,3 @@
 lasso.trimStart = function (string) {
-  var i = 0;
-  var s = string.substr(i, 1);
-  while (s === ' ' || s === '\n' || s === '\t' || s === '\r') {
-    i += 1;
-    s = string.substr(i, 1);
-  }
-  return string.slice(i);
+  return string.replace(/^\s+/, '');
 };
