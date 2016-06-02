d141201331d2ce935595cc3c7f8ef72dd8ce6ac5
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-06-02 17:47:22 -0400

    Replaced missed tabs with spaces

--- src/string/differentWords.js
+++ src/string/differentWords.js
@@ -1,36 +1,36 @@
 function differentWords (a, b) {
-  var z = [];
-  var index;
-  var x;
-  var y;
-  var i;
-  var n;
-  var j;
-  var m;
+	var z = [];
+	var index;
+	var x;
+	var y;
+	var i;
+	var n;
+	var j;
+	var m;
 
-  a = a.match(/[a-zA-Z0-9 ]+/g).join(' ').replace(/[ ]+/g, ' ').split(' ');
-  b = b.match(/[a-zA-Z0-9 ]+/g).join(' ').replace(/[ ]+/g, ' ').split(' ');
+	a = a.match(/[a-zA-Z0-9 ]+/g).join(' ').replace(/[ ]+/g, ' ').split(' ');
+	b = b.match(/[a-zA-Z0-9 ]+/g).join(' ').replace(/[ ]+/g, ' ').split(' ');
 
-  for (i = 0, n = a.length; i < n; i++) {
-  	index = b.indexOf(a[i]);
-  	if (index === -1) {
-  		z.push(a[i]);
-  	}
-  	while (index > -1) {
-  		b.splice(index, 1);
-  		index = b.indexOf(a[i]);
-  	}
-  }
+	for (i = 0, n = a.length; i < n; i++) {
+		index = b.indexOf(a[i]);
+	if (index === -1) {
+		z.push(a[i]);
+	}
+	while (index > -1) {
+		b.splice(index, 1);
+		index = b.indexOf(a[i]);
+		}
+	}
 
-  for (i = 0, n = b.length; i < n; i++) {
-  	index = a.indexOf(b[i]);
-  	if (index === -1 && z.indexOf(b[i]) === -1) {
-  		z.push(b[i]);
-  	}
-  	while (index > -1) {
-  		a.splice(index, 1);
-  		index = a.indexOf(b[i]);
-  	}
-  }
-  return z;
+	for (i = 0, n = b.length; i < n; i++) {
+		index = a.indexOf(b[i]);
+		if (index === -1 && z.indexOf(b[i]) === -1) {
+			z.push(b[i]);
+		}
+		while (index > -1) {
+			a.splice(index, 1);
+			index = a.indexOf(b[i]);
+		}
+	}
+	return z;
 }
