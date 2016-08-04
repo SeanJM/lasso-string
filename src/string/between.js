0707d8c3ee68d0b590260924720fe3d3b68ed977
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-08-04 08:05:45 -0400

    Between had a bug where if the end and start characters weren't of a length of 1 it would always fail.

--- src/string/between.js
+++ src/string/between.js
@@ -1,20 +1,28 @@
 function between (string, start, end) {
+  var startLen = start.length;
+  var endLen = end.length;
   var i = string.indexOf(start);
   var n = 1;
   var o = 1;
+  var esc;
+  var arr;
 
   while (i > -1 && i + n < string.length && o > 0) {
-    if (string[i + n - 1] !== '\\') {
-      if (string[i + n] === start) {
-        o++;
-      } else if (string[i + n] === end) {
-        o--;
-      }
+  	esc = string[i + n - 1] !== '\\';
+    if (esc && string.substr(i + n, startLen) === start) {
+      o++;
+    } else if (esc && string.substr(i + n, endLen) === end) {
+      o--;
     }
     n++;
   }
 
+  arr = [ string.substr(i, n + 1), string.substr(i + 1, n - 2) ];
+
+  arr.start = i;
+  arr.end = n + 1;
+
   return o === 0
-    ? [ string.substr(i, n + 1), string.substr(i + 1, n - 2) ]
+    ? arr
     : false;
 }
